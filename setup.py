4cab0aea78d66688cf5b4e6eeb2b6a2442725f14
joaquin <j.amatrodrigo@gmail.com>
2021-02-14 22:57:33 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="skforecast",
+    version="0.0.1",
+    author="Joaquin Amat Rodrigo",
+    author_email="j.amatrodrigo@gmail.com",
+    description="Forecasting time series with scikitlearn regressors",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JoaquinAmatRodrigo/skforecast",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
