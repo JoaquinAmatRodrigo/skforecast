1bccb267d500a07050141ccf1b2eafe6467c914e
Joaquín Amat Rodrigo <JoaquinAmatRodrigo@users.noreply.github.com>
2021-02-19 11:51:05 +0100

    added requirements.txt

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+################################################################################
+#                               skforecast                                     #
+#                                                                              #
+# This work by Joaquín Amat Rodrigo is licensed under a Creative Commons       #
+# Attribution 4.0 International License.                                       #
+################################################################################
+
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -9,8 +16,6 @@ setuptools.setup(
     author="Joaquin Amat Rodrigo",
     author_email="j.amatrodrigo@gmail.com",
     description="Forecasting time series with scikitlearn regressors",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/JoaquinAmatRodrigo/skforecast",
     packages=setuptools.find_packages(),
     classifiers=[]
