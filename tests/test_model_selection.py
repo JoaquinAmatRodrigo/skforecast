d9c666678a263588061750f1a2552560464f454b
joaquin <j.amatrodrigo@gmail.com>
2021-03-15 21:51:50 +0100

    Update test_model_selection.py

--- tests/test_model_selection.py
+++ tests/test_model_selection.py
@@ -1,7 +1,7 @@
 import numpy as np
 from skforecast.model_selection import grid_search_forecaster
 from skforecast.model_selection import time_series_spliter
-from skforecast.model_selection import ts_cv_forecaster
+from skforecast.model_selection import cv_forecaster
 
 def test_time_series_spliter():
     '''
@@ -10,3 +10,22 @@ def test_time_series_spliter():
     results = time_series_spliter(np.arange(5), initial_train_size=3, steps=1)
     
     assert(list(results) == [(range(0, 3), range(3, 4)), (range(0, 4), range(4, 5))])
+    
+    
+def test_time_series_spliter_exceptions():
+    '''
+    Check exceptions for time_series_spliter().
+    '''
+    
+    with pytest.raises(Exception):
+        list(time_series_spliter([1, 2, 3], initial_train_size=3, steps=1))
+
+    with pytest.raises(Exception):
+        list(time_series_spliter(y=100, initial_train_size=3, steps=1))
+        
+    with pytest.raises(Exception):
+        list(time_series_spliter(
+                y=np.arange(10).reshape(5,2),
+                initial_train_size=3,
+                steps=1)
+        )
