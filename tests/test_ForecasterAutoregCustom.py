3dd7b033e51cf7decc2de816f9b0dfa19a33ee02
Joaquín Amat Rodrigo <j.amatrodrigo@gmail.com>
2021-08-15 21:45:48 +0200

    added pandas dataframe as exog input

--- tests/test_ForecasterAutoregCustom.py
+++ tests/test_ForecasterAutoregCustom.py
@@ -197,6 +197,29 @@ def test_predict_exception_when_exog_lenght_is_less_than_steps():
     forecaster.fit(y=np.arange(50), exog=np.arange(50))
     with pytest.raises(Exception):
         forecaster.predict(steps=10, exog=np.arange(5))
+
+
+def test_predict_exception_when_exog_passed_in_predict_has_different_columns_than_exog_used_to_fit_nparray():
+
+    forecaster = ForecasterAutoregCustom(
+                        regressor      = LinearRegression(),
+                        fun_predictors = create_predictors,
+                        window_size    = 5
+                 )
+    forecaster.fit(y=np.arange(10), exog=np.arange(30).reshape(-1, 3))
+    with pytest.raises(Exception):
+        forecaster.predict(steps=10, exog=np.arange(30).reshape(-1, 2))
+
+def test_predict_exception_when_exog_passed_in_predict_has_different_columns_than_exog_used_to_fit_pdDataDrame():
+
+    forecaster = ForecasterAutoregCustom(
+                        regressor      = LinearRegression(),
+                        fun_predictors = create_predictors,
+                        window_size    = 5
+                 )
+    forecaster.fit(y=np.arange(10), exog=pd.DataFrame(np.arange(30).reshape(-1, 3)))
+    with pytest.raises(Exception):
+        forecaster.predict(steps=10, exog=pd.DataFrame(np.arange(30).reshape(-1, 2)))
         
         
 def test_predict_exception_when_last_window_argument_is_not_numpy_array_or_pandas_series():
@@ -406,6 +429,19 @@ def test_check_exog_exception_when_exog_is_pandas_series_and_ref_shape_has_more_
             exog      = pd.Series(np.arange(30)),
             ref_type  = np.ndarray,
             ref_shape = (1, 5)
+        )
+
+def test_check_exog_exception_when_exog_is_pandas_dataframe_with_3_columns_and_ref_shape_has_2_columns():
+    forecaster = ForecasterAutoregCustom(
+                        regressor      = LinearRegression(),
+                        fun_predictors = create_predictors,
+                        window_size    = 5
+                 )
+    with pytest.raises(Exception):
+        forecaster._check_exog(
+            exog      = pd.DataFrame(np.arange(30).reshape(-1, 3)),
+            ref_type  = pd.DataFrame,
+            ref_shape = (10, 2)
         )
 
         
