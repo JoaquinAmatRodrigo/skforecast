843c334c6bb171b108b6655687084252decad114
Joaquín Amat Rodrigo <j.amatrodrigo@gmail.com>
2021-11-12 18:40:13 +0100

    moved staticmethods of ForecasterBase to model utils.py

--- skforecast/ForecasterBase/ForecasterBase.py
+++ skforecast/ForecasterBase/ForecasterBase.py
@@ -6,10 +6,9 @@
 ################################################################################
 # coding=utf-8
 
+from abc import ABC, abstractmethod
 from typing import Union, Dict, List, Tuple, Any
-import warnings
 import logging
-import numpy as np
 import pandas as pd
 
 logging.basicConfig(
@@ -22,25 +21,26 @@ logging.basicConfig(
 #                                ForecasterBase                                #
 ################################################################################
 
-class ForecasterBase():
+class ForecasterBase(ABC):
     '''
     Base class for all forecasters in skforecast. All forecasters should specify
     all the parameters that can be set at the class level in their ``__init__``.     
     '''
 
-
+    @abstractmethod
     def create_train_X_y(
         self,
         y: pd.Series,
         exog: Union[pd.Series, pd.DataFrame]=None
     ) -> Tuple[pd.DataFrame, pd.Series]:
         '''
-        Create training matrices from univariante time series.
+        Create training matrices from univariante time series and exogenous
+        variables.
         
         Parameters
         ----------        
         y : pandas Series
-            Training time series.
+            Time series.
             
         exog : pandas Series, pandas DataFrame, default `None`
             Exogenous variable/s included as predictor/s. Must have the same
@@ -58,7 +58,8 @@ class ForecasterBase():
         
         pass
 
-        
+
+    @abstractmethod
     def fit(
         self,
         y: pd.Series,
@@ -86,7 +87,8 @@ class ForecasterBase():
         
         pass
 
-            
+
+    @abstractmethod        
     def predict(
         self,
         steps: int,
@@ -122,57 +124,8 @@ class ForecasterBase():
 
         pass
 
-    
-    @staticmethod
-    def _check_y(y: Any) -> None:
-        '''
-        Raise Exception if `y` is not pandas Series or if it has missing values.
-        
-        Parameters
-        ----------        
-        y : Any
-            Time series values
-            
-        Returns
-        ----------
-        None
-        
-        '''
-        
-        if not isinstance(y, pd.Series):
-            raise Exception('`y` must be a pandas Series.')
-            
-        if y.isnull().any():
-            raise Exception('`y` has missing values.')
-        
-        return
-        
-        
-    @staticmethod
-    def _check_exog(exog: Any) -> None:
-        '''
-        Raise Exception if `exog` is not pandas Series or DataFrame, or
-        if it has missing values.
-        
-        Parameters
-        ----------        
-        exog :  Any
-            Exogenous variable/s included as predictor/s.
 
-        Returns
-        ----------
-        None
-        '''
-            
-        if not isinstance(exog, (pd.Series, pd.DataFrame)):
-            raise Exception('`exog` must be `pd.Series` or `pd.DataFrame`.')
-
-        if exog.isnull().any().any():
-            raise Exception('`exog` has missing values.')
-                    
-        return
-
-
+    @abstractmethod
     def _check_predict_input(
         self,
         steps: int,
@@ -186,186 +139,7 @@ class ForecasterBase():
         pass    
         
 
-    @staticmethod
-    def _preproces_y(y: pd.Series) -> Union[np.ndarray, pd.Index]:
-        
-        '''
-        Returns values ​​and index of series separately. Index is overwritten
-        according to the next rules:
-            If index is not of type DatetimeIndex, a RangeIndex is created.
-            If index is of type DatetimeIndex and but has no frequency, a
-            RangeIndex is created.
-            If index is of type DatetimeIndex and has frequency, nothing is
-            changed.
-        
-        Parameters
-        ----------        
-        y : pandas Series
-            Time series values
-
-        Returns 
-        -------
-        y_values : numpy ndarray
-            Numpy array with values of `y`.
-
-        y_index : pandas Index
-            Index of of `y` modified according to the rules.
-        '''
-        
-        if isinstance(y.index, pd.DatetimeIndex) and y.index.freq is not None:
-            y_index = y.index
-        elif isinstance(y.index, pd.DatetimeIndex):
-            warnings.warn(
-                '`y` has DatetimeIndex index but no frequency. ',
-                'Index is overwritten with a RangeIndex.'
-            )
-            y_index = pd.RangeIndex(
-                        start = 0,
-                        stop  = len(y),
-                        step  = 1
-                       )
-        else:
-            warnings.warn(
-                '`y` has no DatetimeIndex index. Index is overwritten with a RangeIndex.'
-            )
-            y_index = pd.RangeIndex(
-                        start = 0,
-                        stop  = len(y),
-                        step  = 1
-                       )
-
-        y_values = y.to_numpy()
-
-        return y_values, y_index
-            
-
-    @staticmethod
-    def _preproces_last_window(last_window: pd.Series) -> Union[np.ndarray, pd.Index]:
-        
-        '''
-        Returns values ​​and index of series separately. Index is overwritten
-        according to the next rules:
-            If index is not of type DatetimeIndex, a RangeIndex is created.
-            If index is of type DatetimeIndex and but has no frequency, a
-            RangeIndex is created.
-            If index is of type DatetimeIndex and has frequency, nothing is
-            changed.
-        
-        Parameters
-        ----------        
-        last_window : pandas Series
-            Time series values
-
-        Returns 
-        -------
-        last_window_values : numpy ndarray
-            Numpy array with values of `last_window`.
-
-        last_window_index : pandas Index
-            Index of of `last_window` modified according to the rules.
-        '''
-        
-        if isinstance(last_window.index, pd.DatetimeIndex) and last_window.index.freq is not None:
-            last_window_index = last_window.index
-        else:
-            warnings.warn(
-                '`last_window` has DatetimeIndex index but no frequency. '
-                'Index is overwritten with a RangeIndex.'
-            )
-            last_window_index = pd.RangeIndex(
-                        start = 0,
-                        stop  = len(last_window),
-                        step  = 1
-                       )
-
-        last_window_values = last_window.to_numpy()
-
-        return last_window_values, last_window_index
-        
-
-    @staticmethod
-    def _preproces_exog(
-        exog: Union[pd.Series, pd.DataFrame]
-    ) -> Union[np.ndarray, pd.Index]:
-        
-        '''
-        Returns values ​​and index separately. Index is overwritten according to
-        the next rules:
-            If index is not of type DatetimeIndex, a RangeIndex is created.
-            If index is of type DatetimeIndex and but has no frequency, a
-            RangeIndex is created.
-            If index is of type DatetimeIndex and has frequency, nothing is
-            changed.
-
-        Parameters
-        ----------        
-        exog : pd.Series, pd.DataFrame
-            Exogenous variables
-
-        Returns 
-        -------
-        exog_values : np.ndarray
-            Numpy array with values of `exog`.
-        exog_index : pd.Index
-            Exog index.
-        '''
-        
-        if isinstance(exog.index, pd.DatetimeIndex) and exog.index.freq is not None:
-            exog_index = exog.index
-        else:
-            warnings.warn(
-                ('`exog` has DatetimeIndex index but no frequency. The index is '
-                 'overwritten with a RangeIndex.')
-            )
-            exog_index = pd.RangeIndex(
-                            start = 0,
-                            stop  = len(exog),
-                            step  = 1
-                          )
-
-        exog_values = exog.to_numpy()
-
-        return exog_values, exog_index
-
-    @staticmethod
-    def _expand_index(index: Union[pd.Index, None], steps: int) -> pd.Index:
-        
-        '''
-        Create a new index of lenght `steps` starting and the end of index.
-        
-        Parameters
-        ----------        
-        index : pd.Index, None
-            Index of last window
-        steps: int
-            Number of steps to expand.
-
-        Returns 
-        -------
-        new_index : pd.Index
-        '''
-        
-        if isinstance(index, pd.Index):
-            
-            if isinstance(index, pd.DatetimeIndex):
-                new_index = pd.date_range(
-                                index[-1] + index.freq,
-                                periods = steps,
-                                freq    = index.freq
-                            )
-            elif isinstance(index, pd.RangeIndex):
-                new_index = pd.RangeIndex(
-                                start = index[-1] + 1,
-                                stop  = index[-1] + 1 + steps
-                             )
-        else: 
-            new_index = pd.RangeIndex(
-                            start = 0,
-                            stop  = steps
-                         )
-        return new_index
-    
-    
+    @abstractmethod
     def set_params(self, **params: dict) -> None:
         '''
         Set new values to the parameters of the scikit learn model stored in the
@@ -384,7 +158,8 @@ class ForecasterBase():
         
         pass
         
-        
+
+    @abstractmethod    
     def set_lags(self, lags: int) -> None:
         '''      
         Set new value to the attribute `lags`.
---
+++ skforecast/plot/plot.py
@@ -0,0 +1,64 @@
+import typing
+from typing import Union
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from statsmodels.graphics.tsaplots import plot_acf
+
+def plot_residuals(residuals: Union[np.array, pd.Series]=None,
+                   y_true: Union[np.array, pd.Series]=None,
+                   y_pred: Union[np.array, pd.Series]=None,
+                   fig: matplotlib.figure.Figure=None,
+                   **kwargs) -> None:
+    '''
+    
+    Parameters
+    ----------
+    residuals: np.array, default `None`.
+        Values of residuals. If `None`, residuals are calculated internally using
+        `y_true` and `y_true`.
+        
+    y_true: np.array, default `None`.
+        Ground truth (correct) values. Ignored if residuals is not `None`.
+
+    y_pred: np.array, default `None`. 
+        Values of predictions. Ignored if residuals is not `None`.
+        
+    fig: matplotlib.figure.Figure, default `None`. 
+        Pre-existing fig for the plot. Otherwise, call matplotlib.pyplot.figure()
+        internally.
+        
+    kwargs
+        Other keyword arguments are passed to matplotlib.pyplot.figure()
+        
+    Returns
+    -------
+        None
+    
+    '''
+    
+    if residuals is None and (y_true is None or y_pred is None):
+        raise Exception(
+            "If `residuals` aregument is None then, y_true and y_pred must be provided."
+        )
+        
+    if residuals is None:
+        residuals = y_pred - y_true
+            
+    if fig is None:
+        fig = plt.figure(constrained_layout=True, **kwargs)
+        
+    gs  = matplotlib.gridspec.GridSpec(2, 2, figure=fig)
+    ax1 = plt.subplot(gs[0, :])
+    ax2 = plt.subplot(gs[1, 0])
+    ax3 = plt.subplot(gs[1, 1])
+    
+    ax1.plot(residuals)
+    sns.histplot(residuals, kde=True, bins=30, ax=ax2)
+    plot_acf(residuals, ax=ax3, lags=60)
+    
+    ax1.set_title("Residuals")
+    ax2.set_title("Distribution")
+    ax3.set_title("Autocorrelation")
