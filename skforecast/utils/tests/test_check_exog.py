49e83566147822e4cdd0f62caffa8b129256fb2d
Joaquín Amat Rodrigo <j.amatrodrigo@gmail.com>
2021-11-15 20:41:09 +0100

    update with local changes

---
+++ skforecast/utils/tests/test_check_exog.py
@@ -0,0 +1,29 @@
+# Unit test check_exog
+# ==============================================================================
+import pytest
+import numpy as np
+import pandas as pd
+from skforecast.utils import check_exog
+
+def test_check_exog_exception_when_exog_not_series_or_dataframe():
+    '''
+    Check exception is raised when y is not pandas Series
+    '''
+    with pytest.raises(Exception):
+        check_exog(y=10)
+    
+    with pytest.raises(Exception):
+        check_exog(y=[1, 2, 3])
+    
+    with pytest.raises(Exception):
+        check_exog(y=np.arange(10).reshape(-1, 2))
+
+
+def test_check_exog_exception_when_exog_has_missing_values():
+    '''
+    Check exception is raised when y has missing values
+    '''
+    with pytest.raises(Exception):
+        check_exog(pd.Series([0, 1, None]))
+    with pytest.raises(Exception):
+        check_exog(pd.DataFrame([[1,2], [3, None], [5, 6]]))
---
+++ skforecast/utils/tests/test_preproces_last_window.py
@@ -0,0 +1,54 @@
+# Unit test preprocess_last_window
+# ==============================================================================
+import pytest
+import numpy as np
+import pandas as pd
+from skforecast.utils import preprocess_last_window
+
+def test_output_preprocess_last_window_when_last_window_index_is_DatetimeIndex_and_has_frequecy():
+    '''
+    Test values returned by when last_window is a pandas Series with DatetimeIndex
+    and freq is not None.
+    '''
+    last_window = pd.Series(
+            data = np.arange(3),
+            index = pd.date_range("1990-01-01", periods=3, freq='D')
+        )
+    results = preprocess_last_window(last_window)
+    expected = (np.arange(3),
+                pd.DatetimeIndex(['1990-01-01', '1990-01-02', '1990-01-03'],
+                                 dtype='datetime64[ns]', freq='D')
+               )
+    assert (results[0] == expected[0]).all()
+    assert (results[1] == expected[1]).all()
+    
+
+def test_output_preprocess_last_window_when_last_window_index_is_DatetimeIndex_but_has_not_frequecy():
+    '''
+    Test values returned by when last_window is a pandas Series with DatetimeIndex
+    and freq is None.
+    '''
+    last_window = pd.Series(
+            data = np.arange(3),
+            index = pd.to_datetime(["1990-01-01", "1990-01-02", "1990-01-03"])
+        )
+    
+    results = preprocess_last_window(last_window)
+    expected = (np.arange(3),
+                pd.RangeIndex(start=0, stop=3, step=1)
+               )
+    assert (results[0] == expected[0]).all()
+    assert (results[1] == expected[1]).all()
+    
+    
+def test_output_preprocess_last_window_when_last_window_index_is_not_DatetimeIndex():
+    '''
+    Test values returned by when last_window is a pandas Series without DatetimeIndex.
+    '''
+    last_window = pd.Series(data=np.arange(3))
+    results = preprocess_last_window(last_window)
+    expected = (np.arange(3),
+                pd.RangeIndex(start=0, stop=3, step=1)
+               )
+    assert (results[0] == expected[0]).all()
+    assert (results[1] == expected[1]).all()
