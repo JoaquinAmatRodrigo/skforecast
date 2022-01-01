5c84b43308ca344af36c8ea409f9072c77869b27
Joaquín Amat Rodrigo <j.amatrodrigo@gmail.com>
2022-01-01 19:12:20 +0100

    update init to version 0.4.2

--- tests/test_skforecast.py
+++ tests/test_skforecast.py
@@ -1,7 +1,4 @@
-import sys
-sys.path.insert(1, '/home/ximo/Documents/GitHub/skforecast')
-
 from skforecast import __version__
 
 def test_version():
-    assert __version__ ==  '0.4.2dev'
+    assert __version__ ==  '0.4.2'
