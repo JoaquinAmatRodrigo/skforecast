47670c5e3d0bc99f5b7dfa24891beaed541b6baf
Javier Escobar Ortiz <javier.escobar.ortiz@gmail.com>
2022-02-22 16:11:12 +0100

    update testing
    update testing for preprocess_y, preprocess_exog, preprocess_last_window

--- skforecast/utils/tests/test_preproces_last_window.py
+++ skforecast/utils/tests/test_preproces_last_window.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from skforecast.utils import preprocess_last_window
 
-def test_output_preprocess_last_window_when_last_window_index_is_DatetimeIndex_and_has_frequecy():
+def test_output_preprocess_last_window_when_last_window_index_is_DatetimeIndex_and_has_frequency():
     '''
     Test values returned by when last_window is a pandas Series with DatetimeIndex
     and freq is not None.
@@ -21,9 +21,25 @@ def test_output_preprocess_last_window_when_last_window_index_is_DatetimeIndex_a
                )
     assert (results[0] == expected[0]).all()
     assert (results[1] == expected[1]).all()
-    
 
-def test_output_preprocess_last_window_when_last_window_index_is_DatetimeIndex_but_has_not_frequecy():
+
+def test_output_preprocess_last_window_when_last_window_index_is_RangeIndex():
+    '''
+    Test values returned by when last_window is a pandas Series with RangeIndex
+    '''
+    last_window = pd.Series(
+            data = np.arange(3),
+            index = pd.RangeIndex(start=0, stop=3, step=1)
+        )
+    results = preprocess_last_window(last_window)
+    expected = (np.arange(3),
+                pd.RangeIndex(start=0, stop=3, step=1)
+               )
+    assert (results[0] == expected[0]).all()
+    assert (results[1] == expected[1]).all()
+
+
+def test_output_preprocess_last_window_when_last_window_index_is_DatetimeIndex_but_has_not_frequency():
     '''
     Test values returned by when last_window is a pandas Series with DatetimeIndex
     and freq is None.
@@ -32,7 +48,6 @@ def test_output_preprocess_last_window_when_last_window_index_is_DatetimeIndex_b
             data = np.arange(3),
             index = pd.to_datetime(["1990-01-01", "1990-01-02", "1990-01-03"])
         )
-    
     results = preprocess_last_window(last_window)
     expected = (np.arange(3),
                 pd.RangeIndex(start=0, stop=3, step=1)
@@ -41,9 +56,9 @@ def test_output_preprocess_last_window_when_last_window_index_is_DatetimeIndex_b
     assert (results[1] == expected[1]).all()
     
     
-def test_output_preprocess_last_window_when_last_window_index_is_not_DatetimeIndex():
+def test_output_preprocess_last_window_when_last_window_index_is_not_DatetimeIndex_or_RangeIndex():
     '''
-    Test values returned by when last_window is a pandas Series without DatetimeIndex.
+    Test values returned by when last_window is a pandas Series without DatetimeIndex or RangeIndex.
     '''
     last_window = pd.Series(data=np.arange(3))
     results = preprocess_last_window(last_window)
