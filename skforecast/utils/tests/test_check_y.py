4284f50a9d7ee42421a91c96e0aafa82ebf15c13
JavierEscobarOrtiz <javier.escobar.ortiz@gmail.com>
2022-10-11 16:44:55 +0200

    fix_formats

--- skforecast/utils/tests/test_check_y.py
+++ skforecast/utils/tests/test_check_y.py
@@ -7,9 +7,9 @@ from skforecast.utils import check_y
 
 
 def test_check_y_exception_when_y_not_series():
-    '''
+    """
     Check exception is raised when y is not pandas Series
-    '''
+    """
     with pytest.raises(Exception):
         check_y(y=10)
     
@@ -21,8 +21,8 @@ def test_check_y_exception_when_y_not_series():
 
 
 def test_check_y_exception_when_y_has_missing_values():
-    '''
+    """
     Check exception is raised when y has missing values
-    '''
+    """
     with pytest.raises(Exception):
         check_y(pd.Series([0, 1, None]))
--- skforecast/utils/tests/test_preproces_y.py
+++ skforecast/utils/tests/test_preproces_y.py
@@ -7,10 +7,10 @@ from skforecast.utils import preprocess_y
 
 
 def test_output_preprocess_y_when_y_index_is_DatetimeIndex_and_has_frequency():
-    '''
+    """
     Test values returned by when y is a pandas Series DatetimeIndex and freq is
     not None.
-    '''
+    """
     y = pd.Series(
             data = np.arange(3),
             index = pd.date_range("1990-01-01", periods=3, freq='D')
@@ -20,14 +20,15 @@ def test_output_preprocess_y_when_y_index_is_DatetimeIndex_and_has_frequency():
                 pd.DatetimeIndex(['1990-01-01', '1990-01-02', '1990-01-03'],
                                  dtype='datetime64[ns]', freq='D')
                )
+    
     assert (results[0] == expected[0]).all()
     assert (results[1] == expected[1]).all()
 
 
 def test_output_preprocess_y_when_y_index_is_RangeIndex():
-    '''
+    """
     Test values returned by when y is a pandas Series with RangeIndex
-    '''
+    """
     y = pd.Series(
             data = np.arange(3),
             index = pd.RangeIndex(start=0, stop=3, step=1)
@@ -36,15 +37,16 @@ def test_output_preprocess_y_when_y_index_is_RangeIndex():
     expected = (np.arange(3),
                 pd.RangeIndex(start=0, stop=3, step=1)
                )
+    
     assert (results[0] == expected[0]).all()
     assert (results[1] == expected[1]).all()
 
 
 def test_output_preprocess_y_when_y_index_is_DatetimeIndex_but_has_not_frequency():
-    '''
+    """
     Test values returned by when y is a pandas Series with DatetimeIndex but freq 
     is None.
-    '''
+    """
     y = pd.Series(
             data = np.arange(3),
             index = pd.to_datetime(["1990-01-01", "1990-01-02", "1990-01-03"])
@@ -53,18 +55,20 @@ def test_output_preprocess_y_when_y_index_is_DatetimeIndex_but_has_not_frequency
     expected = (np.arange(3),
                 pd.RangeIndex(start=0, stop=3, step=1)
                )
+    
     assert (results[0] == expected[0]).all()
     assert (results[1] == expected[1]).all()
     
     
 def test_output_preprocess_y_when_y_index_is_not_DatetimeIndex_or_RangeIndex():
-    '''
+    """
     Test values returned by when y is a pandas Series without DatetimeIndex or RangeIndex.
-    '''
+    """
     y = pd.Series(data=np.arange(3), index=['0', '1', '2'])
     results = preprocess_y(y)
     expected = (np.arange(3),
                 pd.RangeIndex(start=0, stop=3, step=1)
                )
+    
     assert (results[0] == expected[0]).all()
     assert (results[1] == expected[1]).all()
