a48520fa11efa7e0e6c3506bb241b687e12f04c0
Joaquin Amat <joraquin.amat@veeva.com>
2022-11-23 13:35:47 +0000

    update check optional dependecies

--- skforecast/model_selection_statsmodels/model_selection_statsmodels.py
+++ skforecast/model_selection_statsmodels/model_selection_statsmodels.py
@@ -8,17 +8,16 @@
 
 from typing import Union, Tuple, Optional
 import logging
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
 from ..utils import check_optional_dependency
 from ..model_selection import time_series_splitter
 from ..model_selection.model_selection import _get_metric
 
 try:
-    import numpy as np
-    import pandas as pd
-    from tqdm import tqdm
     from sklearn.model_selection import ParameterGrid
     from statsmodels.tsa.statespace.sarimax import SARIMAX
-
 except Exception as e:
     package_name = str(e).split(" ")[-1].replace("'", "")
     check_optional_dependency(package_name=package_name)
--- skforecast/plot/plot.py
+++ skforecast/plot/plot.py
@@ -7,14 +7,14 @@
 # coding=utf-8
  
 from typing import Union
+import numpy as np
+import pandas as pd
 from ..utils import check_optional_dependency
 
 try:
     import matplotlib
     import matplotlib.pyplot as plt
     import seaborn as sns
-    import numpy as np
-    import pandas as pd
     from statsmodels.graphics.tsaplots import plot_acf
 except Exception as e:
     package_name = str(e).split(" ")[-1].replace("'", "")
