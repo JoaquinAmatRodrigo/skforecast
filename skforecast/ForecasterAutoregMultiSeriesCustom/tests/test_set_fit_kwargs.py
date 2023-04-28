84d6fd179aecc6f823bcc0246e9edacb83160feb
JavierEscobarOrtiz <javier.escobar.ortiz@gmail.com>
2023-04-28 18:48:33 +0200

    update_testing

--- skforecast/ForecasterAutoregMultiSeriesCustom/tests/test_set_fit_kwargs.py
+++ skforecast/ForecasterAutoregMultiSeriesCustom/tests/test_set_fit_kwargs.py
@@ -1,6 +1,6 @@
-# Unit test set_fit_kwargs ForecasterAutoregCustom
+# Unit test set_fit_kwargs ForecasterAutoregMultiSeriesCustom
 # ==============================================================================
-from skforecast.ForecasterAutoregCustom import ForecasterAutoregCustom
+from skforecast.ForecasterAutoregMultiSeriesCustom import ForecasterAutoregMultiSeriesCustom
 from lightgbm import LGBMRegressor
 
 def create_predictors(y): # pragma: no cover
@@ -10,14 +10,14 @@ def create_predictors(y): # pragma: no cover
 
     lags = y[-1:-6:-1]
 
-    return lags  
+    return lags
 
 
 def test_set_fit_kwargs():
     """
     Test set_fit_kwargs method.
     """
-    forecaster = ForecasterAutoregCustom(
+    forecaster = ForecasterAutoregMultiSeriesCustom(
                      regressor      = LGBMRegressor(),
                      fun_predictors = create_predictors,
                      window_size    = 5,
@@ -27,7 +27,7 @@ def test_set_fit_kwargs():
     new_fit_kwargs = {'categorical_feature': ['exog']}
     forecaster.set_fit_kwargs(new_fit_kwargs)
     results = forecaster.fit_kwargs
-    
+
     expected = {'categorical_feature': ['exog']}
 
     assert results == expected
