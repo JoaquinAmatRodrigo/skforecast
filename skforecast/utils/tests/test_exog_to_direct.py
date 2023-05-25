8938290c38a4afad79d67692757310e835e61b8e
Joaquin Amat <joraquin.amat@veeva.com>
2023-05-25 12:40:26 +0000

    update unit test exog_to_direct

--- skforecast/utils/tests/test_exog_to_direct.py
+++ skforecast/utils/tests/test_exog_to_direct.py
@@ -7,6 +7,7 @@ import pandas as pd
 from pytest import approx
 from skforecast.utils import exog_to_direct
 from skforecast.utils import exog_to_direct_numpy
+from pandas.api.types import CategoricalDtype
 
 
 def test_exog_to_direct_TypeError_when_exog_not_pandas_series_or_dataframe():
@@ -61,20 +62,20 @@ def test_exog_to_direct_when_steps_2_exog_DataFrame_2d_float_int(dtype):
                          'exog_2': np.arange(1000, 1010, dtype=dtype)})
     results = exog_to_direct(exog=exog, steps=2)
     expected = pd.DataFrame(
-                   data = np.array([[100, 101, 1000, 1001],
-                                    [101, 102, 1001, 1002],
-                                    [102, 103, 1002, 1003],
-                                    [103, 104, 1003, 1004],
-                                    [104, 105, 1004, 1005],
-                                    [105, 106, 1005, 1006],
-                                    [106, 107, 1006, 1007],
-                                    [107, 108, 1007, 1008],
-                                    [108, 109, 1008, 1009]]),
+                   data = np.array([[ 100, 1000,  101, 1001],
+                                    [ 101, 1001,  102, 1002],
+                                    [ 102, 1002,  103, 1003],
+                                    [ 103, 1003,  104, 1004],
+                                    [ 104, 1004,  105, 1005],
+                                    [ 105, 1005,  106, 1006],
+                                    [ 106, 1006,  107, 1007],
+                                    [ 107, 1007,  108, 1008],
+                                    [ 108, 1008,  109, 1009]]),
                    index   = pd.RangeIndex(start=1, stop=10, step=1),
-                   columns = ['exog_1_step_1', 'exog_1_step_2', 
-                              'exog_2_step_1', 'exog_2_step_2']
-               ).astype({'exog_1_step_1': dtype, 'exog_1_step_2': dtype,
-                         'exog_2_step_1': dtype, 'exog_2_step_2': dtype})
+                   columns = ['exog_1_step_1', 'exog_2_step_1', 
+                              'exog_1_step_2', 'exog_2_step_2']
+               ).astype({'exog_1_step_1': dtype, 'exog_2_step_1': dtype,
+                         'exog_1_step_2': dtype, 'exog_2_step_2': dtype})
 
     pd.testing.assert_frame_equal(results, expected)
 
@@ -101,11 +102,13 @@ def test_exog_to_direct_when_steps_2_exog_DataFrame_2d_category():
     exog = pd.DataFrame({'exog_1': pd.Categorical(range(10)),
                          'exog_2': pd.Categorical(range(100, 110))})
     results = exog_to_direct(exog=exog, steps=2)
-    expected = pd.DataFrame({'exog_1_step_1': pd.Categorical(range(9), categories=range(10)),
-                             'exog_1_step_2': pd.Categorical(range(1, 10), categories=range(10)),
-                             'exog_2_step_1': pd.Categorical(range(100, 109), categories=range(100, 110)),
-                             'exog_2_step_2': pd.Categorical(range(101, 110), categories=range(100, 110))},
-                             index = pd.RangeIndex(start=1, stop=10, step=1))
+    expected = pd.DataFrame({
+                'exog_1_step_1': pd.Categorical(range(9), categories=range(10)),
+                'exog_2_step_1': pd.Categorical(range(100, 109), categories=range(100, 110)),
+                'exog_1_step_2': pd.Categorical(range(1, 10), categories=range(10)),
+                'exog_2_step_2': pd.Categorical(range(101, 110), categories=range(100, 110))},
+                index = pd.RangeIndex(start=1, stop=10, step=1)
+              )
 
     pd.testing.assert_frame_equal(results, expected)
 
@@ -120,23 +123,29 @@ def test_exog_to_direct_when_steps_2_exog_DataFrame_3d_float_int_category():
                          'exog_3': pd.Categorical(range(100, 110))})
     results = exog_to_direct(exog=exog, steps=2)
     expected = pd.DataFrame(
-                   data = np.array([[100, 101, 1000, 1001],
-                                    [101, 102, 1001, 1002],
-                                    [102, 103, 1002, 1003],
-                                    [103, 104, 1003, 1004],
-                                    [104, 105, 1004, 1005],
-                                    [105, 106, 1005, 1006],
-                                    [106, 107, 1006, 1007],
-                                    [107, 108, 1007, 1008],
-                                    [108, 109, 1008, 1009]]),
+                   data = np.array([[ 100., 1000.,  100.,  101., 1001.,  101.],
+                                    [ 101., 1001.,  101.,  102., 1002.,  102.],
+                                    [ 102., 1002.,  102.,  103., 1003.,  103.],
+                                    [ 103., 1003.,  103.,  104., 1004.,  104.],
+                                    [ 104., 1004.,  104.,  105., 1005.,  105.],
+                                    [ 105., 1005.,  105.,  106., 1006.,  106.],
+                                    [ 106., 1006.,  106.,  107., 1007.,  107.],
+                                    [ 107., 1007.,  107.,  108., 1008.,  108.],
+                                    [ 108., 1008.,  108.,  109., 1009.,  109.]]),
                    index   = pd.RangeIndex(start=1, stop=10, step=1),
-                   columns = ['exog_1_step_1', 'exog_1_step_2', 
-                              'exog_2_step_1', 'exog_2_step_2']
-               ).assign(
-                     exog_3_step_1 = pd.Categorical(range(100, 109), categories=range(100, 110)),
-                     exog_3_step_2 = pd.Categorical(range(101, 110), categories=range(100, 110))
-               ).astype({'exog_1_step_1': float, 'exog_1_step_2': float,
-                         'exog_2_step_1': int, 'exog_2_step_2': int})
+                   columns = ['exog_1_step_1', 'exog_2_step_1', 'exog_3_step_1', 'exog_1_step_2',
+                              'exog_2_step_2', 'exog_3_step_2']
+               ).astype(
+                    {'exog_1_step_1': 'float64',
+                    'exog_2_step_1': 'int64',
+                    'exog_3_step_1': 'int64',
+                    'exog_1_step_2': 'float64',
+                    'exog_2_step_2': 'int64',
+                    'exog_3_step_2': 'int64'}
+               ).astype(
+                    {'exog_3_step_1': CategoricalDtype(categories=[100, 101, 102, 103, 104, 105, 106, 107, 108, 109], ordered=False),
+                     'exog_3_step_2': CategoricalDtype(categories=[100, 101, 102, 103, 104, 105, 106, 107, 108, 109], ordered=False)}
+               )
 
     pd.testing.assert_frame_equal(results, expected)
 
@@ -153,17 +162,17 @@ def test_exog_to_direct_when_steps_3_exog_DataFrame_2d_float_int(dtype):
                          'exog_2': np.arange(1000, 1010, dtype=dtype)})
     results = exog_to_direct(exog=exog, steps=3)
     expected = pd.DataFrame(
-                   data = np.array([[100, 101, 102, 1000, 1001, 1002],
-                                    [101, 102, 103, 1001, 1002, 1003],
-                                    [102, 103, 104, 1002, 1003, 1004],
-                                    [103, 104, 105, 1003, 1004, 1005],
-                                    [104, 105, 106, 1004, 1005, 1006],
-                                    [105, 106, 107, 1005, 1006, 1007],
-                                    [106, 107, 108, 1006, 1007, 1008],
-                                    [107, 108, 109, 1007, 1008, 1009]]),
+                   data = np.array([[ 100, 1000,  101, 1001,  102, 1002],
+                                    [ 101, 1001,  102, 1002,  103, 1003],
+                                    [ 102, 1002,  103, 1003,  104, 1004],
+                                    [ 103, 1003,  104, 1004,  105, 1005],
+                                    [ 104, 1004,  105, 1005,  106, 1006],
+                                    [ 105, 1005,  106, 1006,  107, 1007],
+                                    [ 106, 1006,  107, 1007,  108, 1008],
+                                    [ 107, 1007,  108, 1008,  109, 1009]]),
                    index   = pd.RangeIndex(start=2, stop=10, step=1),
-                   columns = ['exog_1_step_1', 'exog_1_step_2', 'exog_1_step_3', 
-                              'exog_2_step_1', 'exog_2_step_2', 'exog_2_step_3']
+                   columns = ['exog_1_step_1', 'exog_2_step_1', 'exog_1_step_2',
+                              'exog_2_step_2', 'exog_1_step_3', 'exog_2_step_3']
                ).astype({'exog_1_step_1': dtype, 'exog_1_step_2': dtype, 'exog_1_step_3': dtype,
                          'exog_2_step_1': dtype, 'exog_2_step_2': dtype, 'exog_2_step_3': dtype})
 
