97f177fc593e9fc881c85d5ba0acc60347dcb455
Joaquin Amat <joraquin.amat@veeva.com>
2023-06-08 07:56:19 +0000

    update init.py

--- skforecast/model_selection/__init__.py
+++ skforecast/model_selection/__init__.py
@@ -1,1 +1,1 @@
-from .model_selection import backtesting_forecaster, grid_search_forecaster, random_search_forecaster, bayesian_search_forecaster
+from .model_selection import backtesting_forecaster, grid_search_forecaster, random_search_forecaster, bayesian_search_forecaster, _create_backtesting_folds
