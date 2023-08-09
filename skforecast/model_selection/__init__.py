c4bb1da136f35f057169444a6df27a7aec973cbc
Joaquín Amat Rodrigo <JoaquinAmatRodrigo@users.noreply.github.com>
2023-08-10 00:58:36 +0200

    multi line import

--- skforecast/model_selection/__init__.py
+++ skforecast/model_selection/__init__.py
@@ -1,1 +1,7 @@
-from .model_selection import backtesting_forecaster, grid_search_forecaster, random_search_forecaster, bayesian_search_forecaster
+from .model_selection import (
+    backtesting_forecaster,
+    grid_search_forecaster,
+    random_search_forecaster,
+    bayesian_search_forecaster,
+    _create_backtesting_folds
+)
