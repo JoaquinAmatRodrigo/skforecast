d43688634739cc0a99b7e3369e3a1294b3ec861d
Joaquín Amat Rodrigo <JoaquinAmatRodrigo@users.noreply.github.com>
2023-08-27 20:28:38 +0200

    added get_info_criteria

--- skforecast/Sarimax/Sarimax.py
+++ skforecast/Sarimax/Sarimax.py
@@ -799,3 +799,42 @@ class Sarimax(BaseEstimator, RegressorMixin):
         """
 
         return self.sarimax_res.summary(alpha=alpha, start=start)
+    
+    @_check_fitted
+    def get_info_criteria(criteria:str='aic', method:str='standard') -> float:
+        """
+        Get the selected information criteria.
+
+        Parameters
+        ----------
+        criteria : str, default 'aic'
+            The information criteria to compute. Valid options are {'aic', 'bic',
+            'hqic'}.
+        method : str, default 'standard'
+            The method for information criteria computation. Default is 'standard'
+            method; 'lutkepohl' computes the information criteria as in Lütkepohl
+            (2007). See more instatsmodels.tsa.statespace.sarimax.SARIMAXResults.info_criteria
+
+        Returns
+        -------
+        float
+            The value of the selected information criteria.
+
+        """
+
+        if criteria not in ['aic', 'bic', 'hqic']:
+            raise ValueError(
+                f"Invalid value for `criteria`. Valid options are 'aic', 'bic', "
+                f"and 'hqic'."
+            )
+        
+        if method not in ['standard', 'lutkepohl']:
+            raise ValueError(
+                f"Invalid value for `method`. Valid options are 'standard' and "
+                f"'lutkepohl'."
+            )
+        
+        value = self.sarimax_res.info_criteria(criteria=criteria, method=method)
+        
+        return value
+        
