37d6c10e193ee4a16811dfa087bf724c01fdbeb4
Joaquín Amat Rodrigo <JoaquinAmatRodrigo@users.noreply.github.com>
2023-09-07 12:20:24 +0200

    Update extract_dependences.py

--- extract_dependences.py
+++ extract_dependences.py
@@ -1,20 +1,20 @@
 import tomli
 from itertools import chain
 
-with open("./pyproject.toml", mode='rb') as fp:
-    pyproject = tomli.load(fp)
+def main():
+    with open("./pyproject.toml", mode='rb') as fp:
+        pyproject = tomli.load(fp)
 
-dependencies_optional = [v for k, v in pyproject['project']['optional-dependencies'].items() if k != 'docs']
-dependencies_optional = chain(*dependencies_optional)
-dependencies_optional = list(set(dependencies_optional))
-dependences = pyproject['project']['dependencies']
-dependences_all = dependences + dependencies_optional
-dependences_all
+    dependencies_optional = [v for k, v in pyproject['project']['optional-dependencies'].items() if k != 'docs']
+    dependencies_optional = chain(*dependencies_optional)
+    dependencies_optional = list(set(dependencies_optional))
+    dependences = pyproject['project']['dependencies']
+    dependences_all = dependences + dependencies_optional
+    dependences_all
 
-with open("requirements.txt", mode='w') as fp:
-    for dependency in dependences_all:
-        fp.write(f"{dependency}\n")
+    with open("requirements.txt", mode='w') as fp:
+        for dependency in dependences_all:
+            fp.write(f"{dependency}\n")
 
 if __name__ == "__main__":
-    import os
-    os.system("python extract_dependences.py")
+    main()
