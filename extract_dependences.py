b693612fd1bf79cc03acc8313ba8213dc6cc885e
Joaquin Amat <joraquin.amat@veeva.com>
2023-09-07 09:29:18 +0000

    update extract_dependeces.py

--- extract_dependences.py
+++ extract_dependences.py
@@ -4,10 +4,13 @@ from itertools import chain
 with open("./pyproject.toml", mode='rb') as fp:
     pyproject = tomli.load(fp)
 
-dependencies = [v for k, v in pyproject['project']['optional-dependencies'].items() if k != 'docs']
-dependencies = chain(*dependencies)
-dependencies = set(dependencies)
+dependencies_optional = [v for k, v in pyproject['project']['optional-dependencies'].items() if k != 'docs']
+dependencies_optional = chain(*dependencies_optional)
+dependencies_optional = list(set(dependencies_optional))
+dependences = pyproject['project']['dependencies']
+dependences_all = dependences + dependencies_optional
+dependences_all
 
 with open("requirements.txt", mode='w') as fp:
-    for dependency in dependencies:
+    for dependency in dependences_all:
         fp.write(f"{dependency}\n")
