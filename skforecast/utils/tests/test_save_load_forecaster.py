078bfe901e700145abfd66dcf0c90be40cb104de
JavierEscobarOrtiz <javier.escobar.ortiz@gmail.com>
2023-12-08 23:13:37 +0100

    add_testing_save_forecaster

--- skforecast/utils/tests/test_save_load_forecaster.py
+++ skforecast/utils/tests/test_save_load_forecaster.py
@@ -4,15 +4,36 @@ import os
 import re
 import joblib
 import pytest
+import inspect
+import warnings
 import numpy as np
 import pandas as pd
-import warnings
 import skforecast
 from skforecast.ForecasterAutoreg import ForecasterAutoreg
+from skforecast.ForecasterAutoregMultiSeriesCustom import ForecasterAutoregMultiSeriesCustom
 from skforecast.utils import save_forecaster
 from skforecast.utils import load_forecaster
 from skforecast.exceptions import SkforecastVersionWarning
 from sklearn.linear_model import LinearRegression
+
+def create_predictors(y): # pragma: no cover
+    """
+    Create first 2 lags of a time series.
+    """
+    
+    lags = y[-1:-3:-1]
+    
+    return lags
+
+def custom_weights(y): # pragma: no cover
+    """
+    """
+    return np.ones(len(y))
+
+def custom_weights2(y): # pragma: no cover
+    """
+    """
+    return np.arange(1, len(y)+1)
 
 
 def test_save_and_load_forecaster_persistence():
@@ -20,12 +41,12 @@ def test_save_and_load_forecaster_persistence():
     Test if a loaded forecaster is exactly the same as the original one.
     """
     forecaster = ForecasterAutoreg(regressor=LinearRegression(), lags=3)
-    rng = np.random.default_rng(12345)
-    y = pd.Series(rng.normal(size=100))
+    rng = np.random.default_rng(123)
+    y = pd.Series(rng.normal(size=50))
     forecaster.fit(y=y)
-    save_forecaster(forecaster=forecaster, file_name='forecaster.py', verbose=True)
-    forecaster_loaded = load_forecaster(file_name='forecaster.py', verbose=True)
-    os.remove('forecaster.py')
+    save_forecaster(forecaster=forecaster, file_name='forecaster.joblib', verbose=True)
+    forecaster_loaded = load_forecaster(file_name='forecaster.joblib', verbose=True)
+    os.remove('forecaster.joblib')
 
     for key in vars(forecaster).keys():
         attribute_forecaster = forecaster.__getattribute__(key)
@@ -49,7 +70,7 @@ def test_save_and_load_forecaster_SkforecastVersionWarning():
     y = pd.Series(rng.normal(size=100))
     forecaster.fit(y=y)
     forecaster.skforecast_version = '0.0.0'
-    save_forecaster(forecaster=forecaster, file_name='forecaster.py', verbose=False)
+    save_forecaster(forecaster=forecaster, file_name='forecaster.joblib', verbose=False)
 
     warn_msg = re.escape(
         (f"The skforecast version installed in the environment differs "
@@ -59,5 +80,66 @@ def test_save_and_load_forecaster_SkforecastVersionWarning():
          f"This may create incompatibilities when using the library.")
     )
     with pytest.warns(SkforecastVersionWarning, match = warn_msg):
-        load_forecaster(file_name='forecaster.py', verbose=False)
-        os.remove('forecaster.py')
+        load_forecaster(file_name='forecaster.joblib', verbose=False)
+        os.remove('forecaster.joblib')
+
+
+@pytest.mark.parametrize("weight_func", 
+                         [custom_weights, 
+                          {'serie_1': custom_weights, 
+                           'serie_2': custom_weights2}], 
+                         ids = lambda func : f'type: {type(func)}')
+def test_save_forecaster_save_custom_functions(weight_func):
+    """ 
+    Test if custom functions are saved correctly.
+    """
+    forecaster = ForecasterAutoregMultiSeriesCustom(
+                     regressor      = LinearRegression(),
+                     fun_predictors = create_predictors,
+                     window_size    = 3,
+                     weight_func    = weight_func
+                 )
+    save_forecaster(forecaster=forecaster, file_name='forecaster.joblib', 
+                    save_custom_functions=True)
+    os.remove('forecaster.joblib')
+
+    fun_predictors_file = forecaster.fun_predictors.__name__ + '.py'
+    assert os.path.exists(fun_predictors_file)
+    with open(fun_predictors_file, 'r') as file:
+        assert inspect.getsource(forecaster.fun_predictors) == file.read()
+    os.remove(fun_predictors_file)
+    
+    weight_functions = weight_func.values() if isinstance(weight_func, dict) else [weight_func]
+    for weight_func in weight_functions:
+        weight_func_file = weight_func.__name__ + '.py'
+        assert os.path.exists(weight_func_file)
+        with open(weight_func_file, 'r') as file:
+            assert inspect.getsource(weight_func) == file.read()
+        os.remove(weight_func_file)
+
+
+@pytest.mark.parametrize("weight_func", 
+                         [None,
+                          custom_weights, 
+                          {'serie_1': custom_weights, 
+                           'serie_2': custom_weights2}], 
+                         ids = lambda func : f'func: {func}')
+def test_save_forecaster_warning_dont_save_custom_functions(weight_func):
+    """ 
+    Test UserWarning when custom functions are not saved.
+    """
+    forecaster = ForecasterAutoregMultiSeriesCustom(
+                     regressor      = LinearRegression(),
+                     fun_predictors = create_predictors,
+                     window_size    = 3,
+                     weight_func    = weight_func
+                 )
+
+    warn_msg = re.escape(
+        ("Custom functions used to create predictors or weights are not saved. "
+         "To save them, set `save_custom_functions` to `True`.")
+    )
+    with pytest.warns(UserWarning, match = warn_msg):
+        save_forecaster(forecaster=forecaster, file_name='forecaster.joblib', 
+                        save_custom_functions=False)
+        os.remove('forecaster.joblib')
