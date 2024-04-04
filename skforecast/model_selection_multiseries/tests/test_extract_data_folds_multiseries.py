db963793776409cb6da923ea1c9fad97cfe92694
JavierEscobarOrtiz <javier.escobar.ortiz@gmail.com>
2024-04-04 12:02:28 +0200

    update_tests

--- skforecast/model_selection_multiseries/tests/test_extract_data_folds_multiseries.py
+++ skforecast/model_selection_multiseries/tests/test_extract_data_folds_multiseries.py
@@ -107,7 +107,8 @@ def test_extract_data_folds_multiseries_series_DataFrame_exog_None_RangeIndex(dr
                          ids=lambda dropna: f'dropna_last_window: {dropna}')
 def test_extract_data_folds_multiseries_series_DataFrame_with_NaN_exog_RangeIndex(dropna_last_window):
     """
-    Test extract_data_folds_multiseries with series DataFrame and exog None.
+    Test extract_data_folds_multiseries with series DataFrame and exog None with
+    RangeIndex.
     """
     series_nan = series.copy()
     series_nan.loc[:28, 'l2'] = np.nan
@@ -206,3 +207,114 @@ def test_extract_data_folds_multiseries_series_DataFrame_with_NaN_exog_RangeInde
         pd.testing.assert_frame_equal(data_fold[4], expected_data_folds[i][4])
         assert data_fold[5] == expected_data_folds[i][5]
 
+
+def test_extract_data_folds_multiseries_series_DataFrame_with_NaN_exog_DatetimeIndex():
+    """
+    Test extract_data_folds_multiseries with series DataFrame and exog None with
+    DatetimeIndex.
+    """
+    series_nan = series.copy()
+    series_nan.loc[28, 'l2'] = np.nan # 29th value of 'l2' is NaN (affected by window_size=5)
+    series_nan.loc[:15, 'l3'] = np.nan # First 16 values of 'l3' are NaN
+
+    series_nan.index = pd.date_range(start='2020-01-01', periods=50, freq='D')
+    exog_datetime = exog.copy()
+    exog_datetime.index = pd.date_range(start='2020-01-01', periods=50, freq='D')
+
+    # Train, last_window, test_no_gap
+    folds = [
+        [[0, 30], [25, 30], [30, 37]], 
+        [[0, 35], [30, 35], [35, 42]]
+    ]
+    span_index = pd.date_range(start='2020-01-01', periods=50, freq='D')
+    window_size = 5
+
+    data_folds = list(
+        _extract_data_folds_multiseries(
+            series             = series_nan, 
+            folds              = folds, 
+            span_index         = span_index, 
+            window_size        = window_size, 
+            exog               = exog_datetime, 
+            dropna_last_window = True, 
+            externally_fitted  = False
+        )
+    )
+
+    expected_data_folds = [
+        (
+            pd.DataFrame(
+                data = {'l1': np.arange(0, 30, dtype=float),
+                        'l2': list(range(50, 78)) + [np.nan] + [79],
+                        'l3': [np.nan]*16 + list(range(116, 130))
+                },
+                index = pd.date_range(start='2020-01-01', periods=30, freq='D')
+            ).astype({'l2': float, 'l3': float}),
+            pd.DataFrame(
+                data = {'l1': np.arange(25, 30, dtype=float),
+                        'l3': np.arange(125, 130, dtype=float)
+                },
+                index = pd.date_range(start='2020-01-26', periods=5, freq='D')
+            ),
+            ['l1', 'l3'],
+            pd.DataFrame(
+                data = {'exog_1': np.arange(1000, 1030, dtype=float),
+                        'exog_2': np.arange(1050, 1080, dtype=float),
+                        'exog_3': np.arange(1100, 1130, dtype=float)
+                },
+                index = pd.date_range(start='2020-01-01', periods=30, freq='D')
+            ),
+            pd.DataFrame(
+                data = {'exog_1': np.arange(1030, 1037, dtype=float),
+                        'exog_2': np.arange(1080, 1087, dtype=float),
+                        'exog_3': np.arange(1130, 1137, dtype=float)
+                },
+                index = pd.date_range(start='2020-01-31', periods=7, freq='D')
+            ),
+            folds[0]
+        ),
+        (
+            pd.DataFrame(
+                data = {'l1': np.arange(0, 35, dtype=float),
+                        'l2': list(range(50, 78)) + [np.nan] + list(range(79, 85)),
+                        'l3': [np.nan]*16 + list(range(116, 135))
+                },
+                index = pd.date_range(start='2020-01-01', periods=35, freq='D')
+            ).astype({'l2': float}), 
+            pd.DataFrame(
+                data = {'l1': np.arange(30, 35, dtype=float),
+                        'l2': np.arange(80, 85, dtype=float),
+                        'l3': np.arange(130, 135, dtype=float)
+                },
+                index = pd.date_range(start='2020-01-31', periods=5, freq='D')
+            ),
+            ['l1', 'l2', 'l3'],
+            pd.DataFrame(
+                data = {'exog_1': np.arange(1000, 1035, dtype=float),
+                        'exog_2': np.arange(1050, 1085, dtype=float),
+                        'exog_3': np.arange(1100, 1135, dtype=float)
+                },
+                index = pd.date_range(start='2020-01-01', periods=35, freq='D')
+            ),
+            pd.DataFrame(
+                data = {'exog_1': np.arange(1035, 1042, dtype=float),
+                        'exog_2': np.arange(1085, 1092, dtype=float),
+                        'exog_3': np.arange(1135, 1142, dtype=float)
+                },
+                index = pd.date_range(start='2020-02-05', periods=7, freq='D')
+            ),
+            folds[1]
+        )
+    ]
+
+    for i, data_fold in enumerate(data_folds):
+
+        assert isinstance(data_fold, tuple)
+        assert len(data_fold) == 6
+        
+        pd.testing.assert_frame_equal(data_fold[0], expected_data_folds[i][0])
+        pd.testing.assert_frame_equal(data_fold[1], expected_data_folds[i][1])
+        assert data_fold[2] == expected_data_folds[i][2]
+        pd.testing.assert_frame_equal(data_fold[3], expected_data_folds[i][3])
+        pd.testing.assert_frame_equal(data_fold[4], expected_data_folds[i][4])
+        assert data_fold[5] == expected_data_folds[i][5]
