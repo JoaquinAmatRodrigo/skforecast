17a13bda9baab4fc37e66c05c3a13611646f9817
Joaquin Amat <joraquin.amat@veeva.com>
2024-10-04 09:23:24 +0000

    ongoing

--- skforecast/model_selection/tests/test_backtesting_forecaster.py
+++ skforecast/model_selection/tests/test_backtesting_forecaster.py
@@ -2,16 +2,14 @@
 # ==============================================================================
 import re
 import pytest
-import numpy as np
-import pandas as pd
 from sklearn.linear_model import Ridge
 from skforecast.ForecasterAutoregDirect import ForecasterAutoregDirect
 from skforecast.ForecasterAutoregMultiSeries import ForecasterAutoregMultiSeries
 from skforecast.model_selection import backtesting_forecaster
+from skforecast.model_selection._split import TimeSeriesFold
 
 # Fixtures
 from .fixtures_model_selection import y
-from .fixtures_model_selection import exog
 
 
 def test_backtesting_forecaster_TypeError_when_forecaster_not_supported_types():
@@ -23,30 +21,36 @@ def test_backtesting_forecaster_TypeError_when_forecaster_not_supported_types():
                     regressor = Ridge(random_state=123),
                     lags      = 2
                  )
-
     forecaters_allowed = [
         'ForecasterAutoreg', 
         'ForecasterAutoregDirect',
         'ForecasterEquivalentDate'
     ]
-
+    cv = TimeSeriesFold(
+            steps                 = 3,
+            initial_train_size    = len(y[:-12]),
+            window_size           = None,
+            differentiation       = None,
+            refit                 = False,
+            fixed_train_size      = True,
+            gap                   = 0,
+            skip_folds            = None,
+            allow_incomplete_fold = True,
+            return_all_indexes    = False,
+        )
     err_msg = re.escape(
         (f"`forecaster` must be of type {forecaters_allowed}, for all other types of "
          f" forecasters use the functions available in the other `model_selection` "
          f"modules.")
     )
+    
     with pytest.raises(TypeError, match = err_msg):
         backtesting_forecaster(
             forecaster            = forecaster,
             y                     = y,
-            steps                 = 3,
+            cv                    = cv,
             metric                = 'mean_absolute_error',
-            initial_train_size    = len(y[:-12]),
-            fixed_train_size      = False,
-            gap                   = 0,
-            allow_incomplete_fold = False,
             exog                  = None,
-            refit                 = False,
             verbose               = False,
             show_progress         = False
         )
@@ -62,27 +66,30 @@ def test_backtesting_forecaster_ValueError_when_ForecasterAutoregDirect_not_enou
                     lags      = 5,
                     steps     = 5
                  )
-    
-    gap = 5
-    steps = 1
-    
+    cv = TimeSeriesFold(
+            steps                 = 1,
+            initial_train_size    = len(y[:-12]),
+            window_size           = None,
+            differentiation       = None,
+            refit                 = False,
+            fixed_train_size      = True,
+            gap                   = 5,
+            skip_folds            = None,
+            allow_incomplete_fold = True,
+            return_all_indexes    = False,
+        )
     err_msg = re.escape(
         (f"When using a ForecasterAutoregDirect, the combination of steps "
-         f"+ gap ({steps + gap}) cannot be greater than the `steps` parameter "
+         f"+ gap ({cv.steps + cv.gap}) cannot be greater than the `steps` parameter "
          f"declared when the forecaster is initialized ({forecaster.steps}).")
     )
     with pytest.raises(ValueError, match = err_msg):
         backtesting_forecaster(
             forecaster            = forecaster,
             y                     = y,
-            steps                 = steps,
             metric                = 'mean_absolute_error',
-            initial_train_size    = len(y[:-12]),
-            fixed_train_size      = False,
-            gap                   = gap,
-            allow_incomplete_fold = False,
             exog                  = None,
-            refit                 = False,
+            cv                    = cv,
             verbose               = False,
             show_progress         = False
         )
